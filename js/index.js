344db55ab47653d6d66cc44662172eb20967a198
Hailagaz <omentielvo.lumenn@gmail.com>
2022-07-14 18:10:34 +0300

    Add live testimonials

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+	// ----------------------------------------
+	// Section: Burger menu
 	let menuBtn = document.querySelector('#menu-toggle');
 	let menu = document.querySelector('.header__list');
 
@@ -10,10 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		menu.classList.toggle('active');
 		menuBtn.classList.toggle('active');
 	});
-
-	// if(window.location.hash) {
-	//     document.querySelector(window.location.hash).scrollIntoView();
-	// }
+	// ----------------------------------------
 
 	// ----------------------------------------
 	// Section: Smooth scroll to chosen section
@@ -34,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
 					behavior: "smooth"
 				});
 				e.preventDefault();
-				
+
 			}
 
 			menu.classList.toggle('active');
@@ -43,7 +42,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 	// ----------------------------------------
 
-	// ----Prising switch----
+	// ----------------------------------------
+	// Section: Pricing switch action
 	let plan = document.getElementById('planCheck');
 	let periods = document.querySelectorAll('.pricing__plan-period');
 	let prices = document.querySelectorAll('.pricing__plan-cash');
@@ -59,4 +59,70 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 	onSwitchClick();
+	// ----------------------------------------
+
+	// ----------------------------------------
+	// Section: Testimonials
+	fetch("js/testimonials.json")
+		.then((response) => response.json())
+		.then((data) => {
+			let slides = '';
+
+			for (let testimonial of data) {
+				slides += `
+					<div class="customer__testimonial">
+						<div class="customer__testimonial-user">
+							<div class="customer__testimonial-photo">
+								<img src="${testimonial.image}" alt="Customer photo">
+							</div>
+							<div class="customer__testimonial-info">
+								<div class="customer__testimonial-name">
+									<h4>${testimonial.name}</h4>
+								</div>
+								<div class="customer__testimonial-position">
+									<p>${testimonial.position}</p>
+								</div>
+							</div>
+						</div>
+						<div class="customer__testimonial-text">
+							<p>${testimonial.testimonial}</p>
+						</div>
+						<div class="customer__testimonial-stars">
+							<img src="${testimonial.stars}>
+						</div>
+					</div>
+			`;
+			}
+			document.querySelector('.swiper').innerHTML = `
+                    <div class="swiper-wrapper">
+                        ${slides}
+                    </div>
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                `;
+				
+			// SWIPER
+			const swiper = new Swiper('.swiper', {
+				// Optional parameters
+				//direction: 'vertical',
+				loop: true,
+				slidesPerView: 3,
+				spaceBetween: 30,
+				// If we need pagination
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				// Navigation arrows
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+			});
+		})
+		.catch((err) => {
+			document.querySelector('.swiper').innerHTML = '<p>Sorry, oops!</p>';
+		});
+	// ----------------------------------------
 })
